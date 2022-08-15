24d50c2c62dc185ddf344f1495c77b5c6e6cbb21
132nd-Professor <132nd-Professor>
2022-08-15 20:35:10 +0200

    clean up import

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ mod writer;
 
 use anyhow::Result;
 
-use crate::writer::OutputData;
-
 fn main() -> Result<()> {
     let (input_filename, is_zip) = reader::find_input_file()?;
     println!("Processing {}", input_filename);
@@ -17,7 +15,8 @@ fn main() -> Result<()> {
     let (header, body) = processor::split_into_header_and_body(lines)?;
     let coalition_per_line = processor::divide_body_by_coalition(&body)?;
 
-    let output_data = OutputData::new(input_filename, is_zip, header, body, coalition_per_line);
+    let output_data =
+        writer::OutputData::new(input_filename, is_zip, header, body, coalition_per_line);
     output_data.save_to_disk()?;
 
     Ok(())
